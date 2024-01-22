794c4d6abd2dfb5e3a9e456e0f6f1d60b7c3ed9c
asuzukosi <keloasuzu@yahoo.com>
2024-01-22 22:17:31 +0000

    feat: handle addition of server actions to mutate the data

--- src/prompt_store/nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ src/prompt_store/nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -1,5 +1,44 @@
-export default function Page(){
-    return (<div>
-        Invoice page
-    </div>)
+import Pagination from "@/app/ui/invoices/pagination"
+import Search from "@/app/ui/search"
+import Table from "@/app/ui/invoices/table"
+import { CreateInvoice } from "@/app/ui/invoices/buttons"
+import { lusitana } from "@/app/ui/fonts"
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons"
+import { Suspense } from "react"
+import { fetchInvoicesPages } from "@/app/lib/data"
+
+interface SearchParams {
+    query?: string
+    page?: string
+}
+
+interface PageProps {
+    searchParams: SearchParams
+}
+
+export default async function Page({searchParams} : PageProps){
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+    const totalPages = await fetchInvoicesPages(query);
+
+    return (
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`${lusitana.className} text-2xl`}>
+                    Invoices
+                </h1>
+            </div>
+            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+                <Search placeholder="Search Invoices...."/>
+                <CreateInvoice />
+            </div>
+            <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+                    <Table query={query} currentPage={currentPage} />
+                </Suspense> 
+           
+            <div className="mt-5 flex w-full justify-center">
+                <Pagination totalPages={totalPages} />
+            </div>
+        </div>
+    )
 }
--- src/prompt_store/nextjs-dashboard/app/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Page() {
-    return (<div>
-        <p>Dashboard Page</p>
-    </div>)
-}
